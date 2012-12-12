eaf8290da9691f6d41dcf167a9349d1554622cf0
Hervé BREDIN <bredin@limsi.fr>
2012-12-12 15:50:52 +0100

    Back to os.system

--- pyjob/templates/qsub.py
+++ pyjob/templates/qsub.py
@@ -9,11 +9,13 @@
 from pyjob import Grid
 from pyjob import CommandTemplate
 import os
+import sys
 import subprocess
 
 execfile('$config')
 grid_search = Grid(grid_params)
 this_job_params = grid_search[int(os.environ['SGE_TASK_ID'])]
+
 command = CommandTemplate('$command').substitute(this_job_params)
-p = subprocess.Popen(command, shell=True)
+os.system(command)
 
